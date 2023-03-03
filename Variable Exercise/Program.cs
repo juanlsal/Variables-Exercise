7134e1f9faad59d577757a291fd35a28cafda153
juan lopez salamanca <juanlopezsalamanca1@gmail.com>
2023-03-03 10:27:03 -0600

    adding

---
+++ Variable Exercise/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Variable_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Darrell";
+
+            int myAge;
+            
+            myAge = 10; 
+
+            char firstinitial = 'A';
+
+            bool isSmart = false;
+
+            double myWeight = 54.3; //you can add "d" to the ned however it's not needed. Its understood to be a double
+
+            decimal myHeight = 20.5m; //must have a "m" at the end or error will appear.
+                                     
+            Console.WriteLine($"you don't know me... I AM {MyName}, the strongest {myAge} year old ever! I am {myWeight} pounds of pure steel and have an impressive height of {myHeight} cm. YOU BETTER RUN!");
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
